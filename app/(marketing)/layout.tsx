7b0724f64b9a1ab9c6c54db77eaa6d7c5c633e86
ahmed said <primaryahmedsaid@gmail.com>
2023-11-17 06:27:34 +0200

    ui fixes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "./_components/footer";
 
 export default function MarketingLayout({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-col min-h-[100svh] bg-slate-50">
+    <div className="flex flex-col min-h-[100svh] bg-muted">
       <Navbar />
       <main className="flex-1 container">{children}</main>
       <Footer />
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,12 +4,16 @@ import RedirectLink from "../_components/redirect-link";
 import { useAuth } from "@clerk/nextjs";
 
 export default function MarketingPage() {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, orgId } = useAuth();
+
+  let session = "/sign-up";
+  if (isSignedIn && !orgId) session = "/select-org";
+  if (isSignedIn && orgId) session = `/organization/${orgId}`;
 
   return (
     <div className="container mt-20 md:mt-28 relative space-y-6 text-center">
-      <div className="circle absolute w-32 h-32 md:left-2/3 md:top-28"></div>
-
+      <div className="circle absolute w-32 h-32 right-0 opacity-25 md:opacity-60"></div>
+      <div className="circle absolute w-32 h-32 bottom-0 opacity-25 md:opacity-60"></div>
       <div className="text-sm bg-amber-100 text-amber-700 inline-block p-2 shadow-sm rounded-full font-semibold">
         <Medal className="inline-block h-5 w-5 mr-2" />
         NO 1 TASK MANAGMENT
@@ -26,7 +30,7 @@ export default function MarketingPage() {
         accomplish it all with Taskify.
       </p>
       <RedirectLink
-        href={isSignedIn ? "dashboard" : "dashboard/sign-up"}
+        href={session}
         title={isSignedIn ? "Dashboard" : "Get started for free"}
         size="lg"
       />
