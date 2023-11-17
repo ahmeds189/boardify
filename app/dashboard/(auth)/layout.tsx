8654d52955741ebf68b4935b4eb4ee22f5c4dc51
ahmed said <primaryahmedsaid@gmail.com>
2023-11-17 06:29:57 +0200

    fix foldre structure & ui fixes

--- app/dashboard/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { type PropsWithChildren } from "react";
-
-export default function AuthLayout({ children }: PropsWithChildren) {
-  return (
-    <div className="flex justify-center items-center min-h-[100svh]">
-      {children}
-    </div>
-  );
-}
