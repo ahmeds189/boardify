7d557779094ecdb2a8b3c90816da774b38eb4e86
ahmed said <primaryahmedsaid@gmail.com>
2023-11-17 06:23:56 +0200

    auth flow logic

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,24 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    // handle redirect if no session
+    if (!auth.userId && !auth.isPublicRoute)
+      return redirectToSignIn({ returnBackUrl: req.url });
+
+    // handle if there is session but no org selected
+    if (
+      auth.userId &&
+      !auth.orgId &&
+      req.nextUrl.pathname !== "/select-org" &&
+      req.nextUrl.pathname !== "/"
+    ) {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
